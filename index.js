25cabb48cf303963aa081c6c0757bd7b0ef06ae5
Jannieca Camba <jpcamba@users.noreply.github.com>
2020-03-29 21:56:38 +0800

    Easier questions lmao

--- index.js
+++ index.js
@@ -1,100 +1,100 @@
 var index = -1;
 var verses = [
   {
-    verse: "For whoever calls on the _____ shall be saved. (Romans 10:13)",
-    answer: "name of the Lord"
+    verse: "For whoever calls on the _____ of the Lord shall be saved. (Romans 10:13)",
+    answer: "name"
   },
   {
-    verse: "For my yoke is easy and _____. (Matthew 11:30)",
-    answer: "my burden is light"
+    verse: "For my yoke is easy and my burden is _____. (Matthew 11:30)",
+    answer: "light"
   },
   {
-    verse: "You are the light of the world. A city that is set on a hill _____. (Matthew 5:14)",
-    answer: "cannot be hidden"
+    verse: "You are the light of the world. A city that is set on a hill cannot be _____. (Matthew 5:14)",
+    answer: "hidden"
   },
   {
     verse: "Pray without _____ (1 Thessalonians 5:17)",
     answer: "ceasing"
   },
   {
-    verse: "This is the day the Lord has made; we will rejoice and _____. (Psalm 118:24)",
-    answer: "be glad in it"
+    verse: "This is the day the Lord has made; we will rejoice and be _____ in it. (Psalm 118:24)",
+    answer: "glad"
   },
   {
-    verse: "In everything _____, for this is the will of God in Christ Jesus for you. (1 Thessalonians 5:18)",
-    answer: "give thanks"
+    verse: "In everything give _____, for this is the will of God in Christ Jesus for you. (1 Thessalonians 5:18)",
+    answer: "thanks"
   },
   {
-    verse: "Your word is a _____ and a light to my path. (Psalm 119:105)",
-    answer: "lamp to my feet"
+    verse: "Your word is a _____ to my feet and a light to my path. (Psalm 119:105)",
+    answer: "lamp"
   },
   {
-    verse: "Believe on the Lord Jesus Christ, and you will be saved, you and _____. (Acts 16:31)",
-    answer: "your household"
+    verse: "Believe on the Lord Jesus Christ, and you will be saved, you and your _____. (Acts 16:31)",
+    answer: "household"
   },
   {
-    verse: "Delight yourself in the Lord, and He shall give you the _____. (Psalm 37:4)",
-    answer: "desires of your heart"
+    verse: "Delight yourself in the Lord, and He shall give you the _____ of your heart. (Psalm 37:4)",
+    answer: "desires"
   },
   {
     verse: "Set your mind on things _____, not on things on the earth. (Colossians 3:2)",
     answer: "above"
   },
   {
-    verse: "For all have sinned and _____ of God. (Romans 3:23)",
-    answer: "fall short of the glory"
+    verse: "For all have sinned and fall short of the _____ of God. (Romans 3:23)",
+    answer: "glory"
   },
   {
-    verse: "Oh, give thanks to the Lord, for He is good! For His mercy _____. (Psalm 136:1)",
-    answer: "endures forever"
+    verse: "Oh, give thanks to the Lord, for He is good! For His mercy endures _____. (Psalm 136:1)",
+    answer: "forever"
   },
   {
-    verse: "The Lord is my light and my salvation - _____? (Psalm 27:1)",
-    answer: "whom shall I fear"
+    verse: "The Lord is my light and my salvation - whom shall I _____? (Psalm 27:1)",
+    answer: "fear"
   },
   {
-    verse: "Resist the devil and he will _____. (James 4:7)",
-    answer: "flee from you"
+    verse: "Resist the devil and he will _____ from you. (James 4:7)",
+    answer: "flee"
   },
   {
-    verse: "Let everything that has breath _____. (Psalm 150:6)",
-    answer: "praise the Lord"
+    verse: "Let everything that has breath _____ the Lord. (Psalm 150:6)",
+    answer: "praise"
   },
   {
-    verse: "Trust in the Lord with all your heart, and lean not _____. (Proverbs 3:5)",
-    answer: "on your own understanding"
+    verse: "Trust in the Lord with all your heart, and lean not on your own _____. (Proverbs 3:5)",
+    answer: "understanding"
   },
   {
-    verse: "We love because He first _____. (1 John 4:19)",
-    answer: "loved us"
+    verse: "We love because He first _____ us. (1 John 4:19)",
+    answer: "loved"
   },
   {
-    verse: "Therefore if anyone is in Christ, he is a _____. (2 Corinthians 5:17)",
-    answer: "new creation"
+    verse: "Therefore if anyone is in Christ, he is a new _____. (2 Corinthians 5:17)",
+    answer: "creation"
   },
   {
-    verse: "Whatever you do, do everything for _____. (1 Corinthians 10:31)",
-    answer: "the glory of God"
+    verse: "Whatever you do, do everything for the _____ of God. (1 Corinthians 10:31)",
+    answer: "glory"
   },
   {
-    verse: "I praise you God, for I am fearfully _____. (Psalm 139:14)",
-    answer: "and wonderfully made"
+    verse: "I praise you God, for I am fearfully and _____ made. (Psalm 139:14)",
+    answer: "wonderfully"
   },
   {
-    verse: "Even though I walk through the valley of the shadow of death, I will _____. (Psalm 23:4)",
-    answer: "fear no evil"
+    verse: "Even though I walk through the valley of the shadow of death, I will fear no _____. (Psalm 23:4)",
+    answer: "evil"
   },
   {
-    verse: "Come to me, all who labor and are heavy laden, and I will _____. (Matthew 11:28)",
-    answer: "give you rest"
+    verse: "Come to me, all who labor and are heavy laden, and I will give you _____. (Matthew 11:28)",
+    answer: "rest"
   },
   {
-    verse: "In the world you will have tribulation. But take heart; I have _____. (John 16:33)",
-    answer: "overcome the world"
+    verse: "In the world you will have tribulation. But take heart; I have _____ the world. (John 16:33)",
+    answer: "overcome"
   },
   {
-    verse: "What then shall we say to these things? If _____, who can be against us? (Romans 8:31)",
-    answer: "God is for us"
+    verse: "What then shall we say to these things? If _____ is for us, who can be against us? (Romans 8:31)",
+    answer: "God"
   }
 ];
 
